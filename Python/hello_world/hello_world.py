2dc971e0bf19e67a9930ba111b37760cc1d5cc35
ibrahim khalil <ibrahim-kh-ps@outlook.com>
2024-04-28 14:32:11 +0300

    all asignmrnt for python

---
+++ Python/hello_world/hello_world.py
@@ -0,0 +1,20 @@
+#1.
+print ("hello world")
+
+#2
+name ="ibrahim"
+print ("hello",name)
+print ("hello" + name)
+
+#3
+name = 42
+print ("hello" , name)
+print ("hello" + str(name))
+
+#4
+fave_food1="sushi"
+fave_food2="batata"
+
+print("i love eat {} and {} .".format(fave_food1,fave_food2))
+print(f"i love eat {fave_food1} and {fave_food2}")
+
