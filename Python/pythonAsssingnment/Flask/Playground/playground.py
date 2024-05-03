18a3b0b84e863325d82bb14bdd2e93ecbcf298c8
ibrahim khalil <ibrahim-kh-ps@outlook.com>
2024-05-03 16:58:52 +0300

    playground assignment

---
+++ Python/pythonAsssingnment/Flask/Playground/playground.py
@@ -0,0 +1,22 @@
+from flask import Flask ,render_template
+app=Flask(__name__)
+
+arry = ["/",'/name/times','/name/times/backgroundcolor' ]
+@app.route('/')
+def level1():
+     return render_template('playground.html', numOfTimes = 3), 
+
+@app.route('/<name>/<times>/')
+def level2(name,times):
+     return render_template ('playground.html' , nameinsidehtml=name , numOfTimes = int(times))
+
+@app.route('/<name>/<times>/<backgroundcolor>')
+def level3(name,times ,backgroundcolor ):
+     return render_template ('playground.html' , nameinsidehtml=name , numOfTimes = int(times),bcolor = backgroundcolor)
+
+
+@app.route('/<path:path>')
+def error(path):
+     return f'this page is not working you can use the folowing paths only {arry}'
+if __name__=="__main__":
+     app.run(debug=True)
