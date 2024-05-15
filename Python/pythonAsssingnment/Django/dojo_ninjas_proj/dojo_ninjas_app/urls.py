73e57393ce43b628cbff69f9e409def5fa793ba6
ibrahim khalil <ibrahim-kh-ps@outlook.com>
2024-05-15 18:44:32 +0300

    Dojos & Ninjas

--- Python/pythonAsssingnment/Django/dojo_ninjas_proj/dojo_ninjas_app/urls.py
+++ Python/pythonAsssingnment/Django/dojo_ninjas_proj/dojo_ninjas_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', views.index),
+    path('addDojo', views.addDojo),
+    path('addNinja',views.addNinja)
 ]
--- Python/pythonAsssingnment/Django/dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ Python/pythonAsssingnment/Django/dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from .models import Dojo ,Ninja
 # Create your views here.
 def index(request):
-     pass
+
+     dojodic={ 'dojo': Dojo.objects.all(),
+     }
+
+     return render(request,'index.html' ,dojodic)
+
+def addDojo(request):
+     # obj = Dojo.objects.all()
+     
+     name = request.POST['name']
+     city = request.POST['city']
+     state=request.POST['state']
+     
+     Dojo.objects.create(name = name,city =city , state = state)
+     return redirect('/')
+
+def addNinja(request):
+
+     firstName = request.POST['firstName']
+     lastName = request.POST['lastName']
+     option = request.POST['dojooption']
+     dojooption =Dojo.objects.get(id = option)
+     Ninja.objects.create(first_name =firstName , last_name =lastName ,dojo = dojooption  )
+     return redirect ('/')
