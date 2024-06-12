ddef00e7203446b0893c871bd578fdaf160a3364
ibrahim khalil <ibrahim-kh-ps@outlook.com>
2024-06-12 15:00:37 +0300

    barista's challenge

--- Java/orderAndItem/Item.java
+++ Java/orderAndItem/Item.java
@@ -1,4 +1,22 @@
 public class Item {
-     String name;
-     Double price;
+     private String name;
+     private Double price;
+     
+     public Item(String name, Double price) {
+          this.name = name;
+          this.price = price;
+     }
+     
+     public String getName() {
+          return name;
+     }
+     public void setName(String name) {
+          this.name = name;
+     }
+     public Double getPrice() {
+          return price;
+     }
+     public void setPrice(Double price) {
+          this.price = price;
+     }
 }
