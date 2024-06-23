74b375677ccbbdc558663d0deb189757012d364b
ibrahim khalil <ibrahim-kh-ps@outlook.com>
2024-06-23 15:37:25 +0300

    Daikichi Path Variables assignment

--- Java-Spring/DaikichiRoutes/DaikichiRoutes/src/main/java/com/example/DaikichiRoutes/ExcellentLuck.java
+++ Java-Spring/DaikichiRoutes/DaikichiRoutes/src/main/java/com/example/DaikichiRoutes/ExcellentLuck.java
@@ -1,5 +1,6 @@
 package com.example.DaikichiRoutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +20,21 @@ public class ExcellentLuck {
 	public String daikichitmw() {
 	                return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	        }
+	
+@RequestMapping("/travil/{cityName}")
+public String city(@PathVariable("cityName") String cityName){
+	return "Congratulations! You will soon travel to  " + cityName;
+}
+
+@RequestMapping("/lotto/{num}")
+public String lotto(@PathVariable("num") int num){
+	if (num%2 ==0) {
+		
+		return "You will take a grand journey in the near future, but be weary of tempting offers" ;
 	}
+	else {
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+	}
+}
+
+}
