b72711c400c316efa9843dda87b3f45c4059b11b
ibrahim khalil <ibrahim-kh-ps@outlook.com>
2024-06-23 12:09:29 +0300

    daikichiRoutes assignment

---
+++ Java/DaikichiRoutes/DaikichiRoutes/src/main/java/com/example/DaikichiRoutes/ExcellentLuck.java
@@ -0,0 +1,22 @@
+package com.example.DaikichiRoutes;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daikichi")
+public class ExcellentLuck {
+	@RequestMapping("")
+	public String hello() {
+	                return "Welcome!";
+	        }
+	
+	@RequestMapping("/today")
+	public String daikichitoday() {
+	                return "Today you will find luck in all your endeavors!";
+	        }
+	@RequestMapping("/tomorrow")
+	public String daikichitmw() {
+	                return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	        }
+	}
