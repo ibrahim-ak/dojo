c1f50ea4e2e53f2639fc5ce06b99505b0f1f8955
ibrahim khalil <ibrahim-kh-ps@outlook.com>
2024-07-31 18:42:36 +0300

    box generator ninja bonus

--- React/BoxGenerator/src/components/Boxgenerator.jsx
+++ React/BoxGenerator/src/components/Boxgenerator.jsx
@@ -4,17 +4,36 @@ import StyledBox from './StyledDev';
 function Boxgenerator() {
      const [color, setColor] = useState("");
      const [colors, setColors] = useState([]);
-
+     const [w, setWidth] = useState();
+     const [h, setHeight] = useState();
      const saveColor = (e) => {
           setColor(e.target.value);
+
+     }
+     const saveHeight = (e) => {
+          setHeight(e.target.value);
+
+     }
+     const saveWidth = (e) => {
+          setWidth(e.target.value);
 
      }
      const handelsubmit = (e) => {
           e.preventDefault();
           // console.log(e);
-          setColors([...colors, color]);
+
+
+          let obj = {
+               c: color
+               , w: w,
+               h: h
+          };
+
+          setColors([...colors, obj]);
           setColor("");
-          // console.log(colors)
+          console.log(colors)
+          setWidth("");
+          setHeight("");
      }
 
 
@@ -24,11 +43,15 @@ function Boxgenerator() {
                     <form onSubmit={handelsubmit}>
                          <label htmlFor="batata">Color</label>
                          <input type="text" name="batata" value={color} onChange={saveColor} />
+                         <label htmlFor="height">height</label>
+                         <input type="text" name="height" value={h} onChange={saveHeight} />
+                         <label htmlFor="width">width</label>
+                         <input type="text" name="width" value={w} onChange={saveWidth} />
                          <button>add</button>
                     </form>
                </div>
                <div style={{ display: 'flex', justifyContent: 'center', maxWidth: '700px', flexWrap: 'wrap' }}>
-                    {colors.map((color, index) => <div key={index} ><StyledBox bgColor={color} /></div>)}
+                    {colors.map((obj, index) => <div key={index}  ><StyledBox bgColor={obj.c} width={obj.w} height={obj.h} /></div>)}
 
                </div>
           </>
--- React/BoxGenerator/src/components/StyledDev.jsx
+++ React/BoxGenerator/src/components/StyledDev.jsx
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 const StyledBox = styled.div`
     border: 1px solid lightgray;
     background: ${props => props.bgColor};
-    width: ${props => props.width || '100px'};
-    height: ${props => props.height || '100px'};
+    width: ${props => props.width +"px"|| '100px'};
+    height: ${props => props.height +"px" || '100px'};
     margin:10px;
 `;
 
