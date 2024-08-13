cb7a26f172591407be29113825207fc8ed1335d1
ibrahim khalil <ibrahim-kh-ps@outlook.com>
2024-08-13 13:57:57 +0300

    adjastment for assignment

--- FullStack mern/Handshake/client/vite.config.js
+++ FullStack mern/Handshake/client/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  host:'0.0.0.0',
   server: {
     host: '0.0.0.0', // Ensure this is a string
     port: 3000,
